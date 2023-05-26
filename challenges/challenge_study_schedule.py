e5e192ebc00bfe3b05ccca1ab0e84a2876171cb9
Rafael Soares <r.soares2015@outlook.com>
2023-05-26 00:28:57 -0300

    feat: Implementa a funcao study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,4 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
-# starting project
+    counter = 0
+    if target_time is None or target_time < 0:
+        return None
+    for period in permanence_period:
+        if type(period[0]) is not int or type(period[1]) is not int:
+            return None
+        if int(period[0]) <= int(target_time) <= int(period[1]):
+            counter += 1
+    return counter
