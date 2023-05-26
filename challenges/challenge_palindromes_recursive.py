1bd26614dd3779531b16ebd3a1e0bae475f624c3
Rafael Soares <r.soares2015@outlook.com>
2023-05-26 02:06:50 -0300

    feat: Implementa a funcao palidromes_recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,7 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    print(low_index, high_index)
+    if len(word) == 0 or word[low_index] != word[high_index]:
+        return False
+    if low_index != high_index and high_index > 0:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    return True
