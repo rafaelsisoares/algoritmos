0ec159ddd624edf7ff1cc85c0c106c9b7bd0f6df
Rafael Soares <r.soares2015@outlook.com>
2023-05-28 00:09:08 -0300

    test: Implementa testes para a funcao encrypt_message

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,18 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
+
+THREE = 3
+FOUR = 4
 
 
 def test_encrypt_message():
-    pass
+    message = 'teste'
+    encrypted_message_odd = encrypt_message(message, THREE)
+    encrypted_message_even = encrypt_message(message, FOUR)
+    inverted_message = encrypt_message(message, -2)
+    assert encrypted_message_odd == 'set_et'
+    assert encrypted_message_even == 'e_tset'
+    assert inverted_message == 'etset'
+
+    with pytest.raises(TypeError):
+        encrypt_message(THREE, message)
