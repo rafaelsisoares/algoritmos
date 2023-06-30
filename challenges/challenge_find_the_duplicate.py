1783a5f132ea3a16e1b93c02aa1efcdb58960906
Rafael Soares <r.soares2015@outlook.com>
2023-06-29 23:58:48 -0300

    feat: Implementa a funcao find_duplicates

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,17 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    nums.sort()
+    if not nums or len(nums) < 2 or not isinstance(nums[0], int):
+        return False
+
+    checked_nums = set()
+    for num in nums:
+        if num < 0:
+            return False
+        elif num in checked_nums:
+            return num
+        else:
+            checked_nums.add(num)
+    return False
+
+if __name__ == '__main__':
+    print(find_duplicate([3, 1, 2, 4, 6, 5, 7, 7, 7, 8]))
